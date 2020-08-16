3054d279a6f89e03edd3c547c7c77b3f3fcfac07
v_lsclan <v_lsclan@tencent.com>
2020-08-16 21:41:51 +0800

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@
 const Koa = require('koa');
 const helmet = require("koa-helmet");
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 
 const Controller = require('./controller');
 
 
 const app = new Koa();
+
+app.use(cors());
 
 app.use(helmet());
 
